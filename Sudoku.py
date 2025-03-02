f3b6e41bb0838209bdb86c49421e9e968f568a64
Poojith <poojithchinnu7@gmail.com>
2025-03-02 20:34:19 +0530

    Sudoku
    The sudoku game which is very hard to solve can be solved

---
+++ Sudoku.py
@@ -0,0 +1,52 @@
+def is_valid(board, row, col, num):
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+    start_row = row - row % 3
+    start_col = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+def print_board(board):
+    for row in board:
+        print(" ".join(str(cell) for cell in row))
+
+board = [
+    [5, 3, 0, 0, 7, 0, 0, 0, 0],
+    [6, 0, 0, 1, 9, 5, 0, 0, 0],
+    [0, 9, 8, 0, 0, 0, 0, 6, 0],
+    [8, 0, 0, 0, 6, 0, 0, 0, 3],
+    [4, 0, 0, 8, 0, 3, 0, 0, 1],
+    [7, 0, 0, 0, 2, 0, 0, 0, 6],
+    [0, 6, 0, 0, 0, 0, 2, 8, 0],
+    [0, 0, 0, 4, 1, 9, 0, 0, 5],
+    [0, 0, 0, 0, 8, 0, 0, 7, 9]
+]
+
+print("Unsolved Sudoku Puzzle:")
+print_board(board)
+
+if solve_sudoku(board):
+    print("\nSolved Sudoku Puzzle:")
+    print_board(board)
+else:
+    print("\nNo solution exists!")
